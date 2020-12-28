a0c813e4dd80f241eb8292aca5656d9c07801136
Daniel Gordon <danielgordon10@gmail.com>
2020-12-28 12:51:08 -0800

    format

--- download_dataset.py
+++ download_dataset.py
@@ -2,5 +2,5 @@ import gdown
 import json
 import tqdm
 
-for path, url in tqdm.tqdm(json.load(open('drive_urls.json')).items()):
+for path, url in tqdm.tqdm(json.load(open("drive_urls.json")).items()):
     gdown.download(url, path, quiet=False)
